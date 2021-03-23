2d0ae9fb7a2998902e4bc6339770cf1cf203217b
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2021-03-23 11:26:32 +0200

    different

--- calc/app/Http/Controllers/BoxController.php
+++ calc/app/Http/Controllers/BoxController.php
@@ -24,7 +24,7 @@ class BoxController extends Controller
      */
     public function create()
     {
-        //
+       return view
     }
 
     /**
--- calc/routes/web.php
+++ calc/routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\CalcControlleNew;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalcController1;
 use App\Http\Controllers\CalcController1Minus;
+use App\Http\Controllers\BoxController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +17,13 @@ Route::get('calc', function () {
 Route::get('calc/{action}/{x}/{y}', [CalcController1Minus::class, 'sum']);
 Route::get('calc', [CalcControlleNew::class, 'show'])->name('show-calc');
 Route::post('calc', [CalcControlleNew::class, 'calc'])->name('domath');
+
+Route::group(['prefix' => 'box'], function () {
+    Route::get('', [BoxController::class, 'index'])->name('box.index');
+    Route::get('create', [BoxController::class, 'create'])->name('box.create');
+    Route::post('store', [BoxController::class, 'store'])->name('box.store');
+    Route::get('edit/{box}', [BoxController::class, 'edit'])->name('box.edit');
+    Route::post('update/{box}', [BoxController::class, 'update'])->name('box.update');
+    Route::post('delete/{box}', [BoxController::class, 'destroy'])->name('box.destroy');
+    Route::get('show/{box}', [BoxController::class, 'show'])->name('box.show');
+});
